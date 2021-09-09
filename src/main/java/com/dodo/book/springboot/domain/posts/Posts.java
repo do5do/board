0453241821faad58d60e76c64bda5b86f82fea03
dodo <do151015@gmail.com>
2021-09-10 02:41:12 +0900

    0910

---
+++ src/main/java/com/dodo/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.dodo.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가 (public Posts() {}와 같은 효과)
+@Entity // JPA의 어노테이션으로 테이블과 링크될 클래스임을 나타냄.
+public class Posts { // 실제 DB의 테이블과 매칭될 클래스 -> Entity 클래스
+    // JPA를 사용하면 DB 데이터에 작업할 경우 실제 쿼리를 날리기보다는, 이 Entity 클래스의 수정을 통해 작업한다.
+    @Id // 해당 테이블의 PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // pk의 생성 규칙을 나타냄. GenerationType.IDENTITY 옵션을 추가해야 auto_increment가 된다.
+    private Long id; // 웬만하면 Entity의 PK는 Long타입의 Auto_increment를 추천한다.
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false) // 사용하는 이유는 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용한다.
+    private String content;
+
+    private String author; // @Column을 굳이 선언하지 않아도 해당 클래스의 필드는 모두 칼럼이 된다.
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스 생성. 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
--- src/test/java/com/dodo/book/springboot/HelloControllerTest.java
+++ src/test/java/com/dodo/book/springboot/HelloControllerTest.java
@@ -21,9 +21,9 @@ public class HelloControllerTest {
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        mvc.perform(get("/hello")) // /hello라는 url로 get 요청
+                .andExpect(status().isOk()) // 위 요청 결과에 따라 상태는 200이며
+                .andExpect(content().string(hello)); // response body에 hello가 있는지 검증
     }
 
     @Test
@@ -32,10 +32,10 @@ public class HelloControllerTest {
         int amount = 1000;
 
         mvc.perform(get("/hello/dto")
-                .param("name", name)
-                .param("amount", String.valueOf(amount)))
+                .param("name", name) // API 테스트할 때 사용될 요청 파라미터 설정.
+                .param("amount", String.valueOf(amount))) // 단 값은 String만 허용된다.
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.name", is(name))) // json 응답값을 필드별로 검증할 수 있는 메소드. $를 기준으로 필드명을 명시
                 .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
